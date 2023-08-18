6d6def54f31ca9f066050cf055d0caeca7b9281e
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-18 10:30:23 -0400

    Assignment complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print(0)
+    else:
+        while len(sequence) < length:
+            next_number = sequence[-1] + sequence[-2]
+            sequence.append(next_number)
+        print(sequence)
+    return sequence
